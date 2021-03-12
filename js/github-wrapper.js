a9a33738bb8e8f5eb5c8fde1e31252202bb9ec1b
Armored Dragon <43324896+Armored-Dragon@users.noreply.github.com>
2021-03-11 22:31:36 -0600

    Simplistic Update
    Everything is now on a single scroll page.
    Email link has been included along with the mailto: link.
    Added Icons to donation options.
    Removed much of the javascript and all of the webrequests.

--- js/github-wrapper.js
+++
@@ -1,8 +0,0 @@
-const githubapi = {
-	getUserPublicRepositories: async (username) => {
-		return JSON.parse(await webRequest('get', `https://api.github.com/users/${username}`));
-	},
-	getUserRepositories: async (username) => {
-		return JSON.parse(await webRequest('get', `https://api.github.com/users/${username}/repos`));
-	}
-}
--- js/index.js
+++ js/index.js
@@ -1,30 +1,11 @@
 const qs = (selector) => document.querySelector(selector);
 const qsa = (selector) => document.querySelectorAll(selector);
 
-(async () => {
-	const page_requests = await Promise.all([githubapi.getUserRepositories('armored-dragon'), githubapi.getUserPublicRepositories('armored-dragon')]);
-	const owner = { repositories: page_requests[0], profile: page_requests[1] };
-	// const pinned_repositories = ['260648901', '315174611'];	// These are repositories that have first dibs on the webpage.
-
-	qs('#owner-pfp').src = owner.profile.avatar_url;
-	qs('#owner-name').innerText = owner.profile.name;
-	qs('#owner-repo-count').innerText = owner.profile.public_repos;
-	qs('#owner-follower-count').innerText = owner.profile.followers;
-
-})();
-
-
-
-function webRequest(type, url) {
-	return new Promise((resolve, reject) => {
-		let newXHR = new XMLHttpRequest() || new window.ActiveXObject('Microsoft.XMLHTTP');
-		newXHR.open(type, url, true);
-		newXHR.send();
-		newXHR.onreadystatechange = function () {
-			if (this.readyState === 4) {
-				if (this.status === 200) return resolve(this.response);
-				else return reject({ error: this.status });
-			}
-		};
-	});
+function decrypt(s) {
+	return s.replace(/[A-Z]/gi, c =>
+		"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"[
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".indexOf(c)]);
 }
+
+qs('#em-link').innerText = decrypt('choyvpznvy@nezberqqentba.pbz');
+qs('#em-link').href = `mailto:${decrypt('choyvpznvy@nezberqqentba.pbz')}`;
